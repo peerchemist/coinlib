bb5b55817abcf3aa43a0255a6e5bad64e927fafe
peerchemist <dx.1178@gmail.com>
2024-11-29 15:04:29 +0100

    add anyPrevOutFlag and anyPrevOutAnyScriptFlag to SigHashType

--- coinlib/lib/src/tx/sighash/sighash_type.dart
+++ coinlib/lib/src/tx/sighash/sighash_type.dart
@@ -2,21 +2,30 @@
 /// Signatures may sign different output and inputs to allow for transaction
 /// modifications. To sign an entire transaction the [all] constructor should be
 /// used.
+/// /// Supports SIGHASH_ANYPREVOUT and SIGHASH_ANYPREVOUTANYSCRIPT.
 class SigHashType {
-
   /// Special value representing the default Schnorr behaviour to sign
   /// everything. This is encoded as an absent byte.
   static const schnorrDefaultValue = 0;
 
   /// Value to sign all outputs
   static const allValue = 1;
+
   /// Value to sign no outputs
   static const noneValue = 2;
+
   /// Value to sign the output at the same index as the input
   static const singleValue = 3;
+
   /// Flag that can be combined with other hash type values to only sign the
   /// input containing the signature
   static const anyOneCanPayFlag = 0x80;
+
+  /// Flag for SIGHASH_ANYPREVOUT (APO)
+  static const anyPrevOutFlag = 0x40;
+
+  /// Flag for SIGHASH_ANYPREVOUTANYSCRIPT (APOAS)
+  static const anyPrevOutAnyScriptFlag = 0xC0;
 
   /// The single byte representation of the sighash type. Use [all], [none],
   /// [single] and [anyOneCanPay] to extract details of the type.
@@ -49,36 +58,39 @@ class SigHashType {
   /// containing the signature will be signed.
   /// If [anyOneCanPay] is false and a Taproot input is being signed, this will
   /// be treated as "SIGHASH_DEFAULT".
-  const SigHashType.all({ bool anyOneCanPay = false })
-    : value = allValue | (anyOneCanPay ? anyOneCanPayFlag : 0);
+  const SigHashType.all({bool anyOneCanPay = false})
+      : value = allValue | (anyOneCanPay ? anyOneCanPayFlag : 0);
 
   /// Sign no outputs. If [anyOneCanPay] is true, then only the input containing
   /// the signature will be signed.
-  const SigHashType.none({ bool anyOneCanPay = false })
-    : value = noneValue | (anyOneCanPay ? anyOneCanPayFlag : 0);
+  const SigHashType.none({bool anyOneCanPay = false})
+      : value = noneValue | (anyOneCanPay ? anyOneCanPayFlag : 0);
 
   /// Sign the output at the same index as the input. If [anyOneCanPay] is true,
   /// then only the input containing the signature will be signed.
-  const SigHashType.single({ bool anyOneCanPay = false })
-    : value = singleValue | (anyOneCanPay ? anyOneCanPayFlag : 0);
+  const SigHashType.single({bool anyOneCanPay = false})
+      : value = singleValue | (anyOneCanPay ? anyOneCanPayFlag : 0);
 
   /// If this is the default hash type for a Schnorr signature.
   bool get schnorrDefault => value == schnorrDefaultValue;
 
   /// All outputs shall be signed
-  bool get all => value == schnorrDefaultValue
-    || (value & ~anyOneCanPayFlag) == allValue;
+  bool get all =>
+      value == schnorrDefaultValue || (value & ~anyOneCanPayFlag) == allValue;
+
   /// No outputs shall be signed
   bool get none => (value & ~anyOneCanPayFlag) == noneValue;
+
   /// Only the output with the same index as the input shall be signed
   bool get single => (value & ~anyOneCanPayFlag) == singleValue;
+
   /// Only the input receiving the signature shall be signed
   bool get anyOneCanPay => (value & anyOneCanPayFlag) != 0;
 
   @override
-  bool operator==(Object other) => other is SigHashType && value == other.value;
+  bool operator ==(Object other) =>
+      other is SigHashType && value == other.value;
 
   @override
   int get hashCode => value;
-
 }
